b6073189c9bb2a706849c28c2abf3f04a34982b9
onehellcat <onehellcat1@gmail.com>
2024-01-30 19:59:08 +0100

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,19 +1,15 @@
 var diceSw = document.getElementById("diceSw");
 var dice_value = diceSw.value;
-var text = diceSw.options[diceSw.selectedIndex].text;
-const button = document.getElementById('roll');
+const button = document.getElementById("roll");
 
+diceSw.onchange = function roll() {
+  dice_value = diceSw.value;
 
-diceSw.onchange = function roll(){
- dice_value = diceSw.value;
-    
-    console.log(dice_value)    
-}
+  console.log(dice_value);
+};
 
-
-
-button.onclick = function roll() {   
-    // Maybe i should replace math.random with crypto.
-    return document.getElementById("display").innerHTML =  Math.floor(Math.random() * dice_value) + 1;
-
+button.onclick = function roll() {
+  // Maybe i should replace math.random with crypto.
+  return (document.getElementById("display").innerHTML =
+    Math.floor(Math.random() * dice_value) + 1);
 };
